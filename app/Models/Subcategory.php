6207ccb12c6d88fb48bffb3e843b2204c816f690
Cheasmile <sokchea1922003@gmail.com>
2026-03-25 01:04:39 +0700

    Fix: use firstOrCreate in Seeder to avoid duplicate error

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -8,24 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Subcategory extends Model
 {
     use HasFactory;
-
     protected $fillable = ['name', 'user_id', 'category_id'];
-
-    // ទំនាក់ទំនងទៅកាន់ User (ម្ចាស់អ្នកបង្កើត)
-    public function user()
-    {
+    public function user(){
         return $this->belongsTo(User::class);
     }
-
-    // ទំនាក់ទំនងទៅកាន់ Category (មេ)
     public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
+{
+    return $this->belongsTo(Category::class);
+}
 
-    // ប្រសិនបើអ្នកមានតារាង Posts ដែលភ្ជាប់ជាមួយ Subcategory
-    public function posts()
-    {
-        return $this->hasMany(Post::class);
-    }
+
 }
